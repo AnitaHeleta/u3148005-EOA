e699c1a32968cec7edff2107a48adbaf2b14c137
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2021-10-01 11:32:36 +0930

    burger menu

--- js/application.js
+++ js/application.js
@@ -1,35 +1,42 @@
-$(document).ready(function () {
-  // API to HTML 1
-  // Accessing an API and returning data
-  // need NMA API key
-  // https://data.nma.gov.au/object?limit=5&medium=*wood&format=simple
-  // docs: https://github.com/NationalMuseumAustralia/Collection-API/wiki/Getting-started
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
 
-  // my api key
-  var key = "aGyOTzAchbsscZt7OnZQ4hSvgSsiW0hW";
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
 
-  // API call
-  var url =
-    "https://data.nma.gov.au/object?limit=5&medium=*glass&format=simple" +
-    "&apikey=" +
-    key;
+// $(document).ready(function () {
+//   // API to HTML 1
+//   // Accessing an API and returning data
+//   // need NMA API key
+//   // https://data.nma.gov.au/object?limit=5&medium=*wood&format=simple
+//   // docs: https://github.com/NationalMuseumAustralia/Collection-API/wiki/Getting-started
 
-  $.getJSON(url, function (apiData) {
-    console.log("im here");
-    console.log(apiData);
+//   // my api key
+//   var key = "aGyOTzAchbsscZt7OnZQ4hSvgSsiW0hW";
 
-    var title = apiData.data[0];
-    var collection = apiData.data[0];
-    var material = apiData.data[0];
-    var physicalDescription = apiData.data[0];
-    var contributor = apiData.data[0];
-    var temporal = apiData.data[0];
+//   // API call
+//   var url =
+//     "https://data.nma.gov.au/object?limit=5&medium=*glass&format=simple" +
+//     "&apikey=" +
+//     key;
 
-    $(".title").html(title.title);
-    $(".collection").html(collection.collection.title);
-    $(".material").html(material.medium[0].title);
-    $(".physicalDescription").html(physicalDescription.physicalDescription);
-    $(".contributor").html(contributor.contributor[0].title);
-    $(".temporal").html(temporal.temporal[0].endDate);
-  });
-});
+//   $.getJSON(url, function (apiData) {
+//     console.log("im here");
+//     console.log(apiData);
+
+//     var title = apiData.data[0];
+//     var collection = apiData.data[0];
+//     var material = apiData.data[0];
+//     var physicalDescription = apiData.data[0];
+//     var contributor = apiData.data[0];
+//     var temporal = apiData.data[0];
+
+//     $(".title").html(title.title);
+//     $(".collection").html(collection.collection.title);
+//     $(".material").html(material.medium[0].title);
+//     $(".physicalDescription").html(physicalDescription.physicalDescription);
+//     $(".contributor").html(contributor.contributor[0].title);
+//     $(".temporal").html(temporal.temporal[0].endDate);
+//   });
+// });
