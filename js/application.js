2aa38cb3174bc02aeee2eb9783e945473fe1da3c
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2021-10-06 15:03:00 +1030

    map adjustment css

--- js/application.js
+++ js/application.js
@@ -39,4 +39,19 @@ $(document).ready(function () {
     $(".contributor").html(contributor.contributor[0].title);
     $(".temporal").html(temporal.temporal[0].endDate);
   });
+
+  var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGFuaXRhIiwiYSI6ImNrdWV4Zzc3ajFvdzgyd3FscXA2ZWRveW4ifQ.ZWWLzaPjFA-TtXzcUZZzeg",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: "your.mapbox.access.token",
+    }
+  ).addTo(mymap);
 });
